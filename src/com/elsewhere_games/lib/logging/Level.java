ef5452eef5aa0eb3eab43d7448c09d5d1338f3e6
Hans Pragt <ElsewhereGames@gmail.com>
2011-07-14 22:08:39 -0700

    Added comments explaining the usage of levels.

--- src/com/elsewhere_games/lib/logging/Level.java
+++ src/com/elsewhere_games/lib/logging/Level.java
@@ -1,5 +1,11 @@
 package com.elsewhere_games.lib.logging;
 
+/**
+ * <p>These are the different levels a log message can assume.</p>
+ * 
+ * @author Hans Pragt
+ *
+ */
 public enum Level {
 	ERROR,
 	WARN,
