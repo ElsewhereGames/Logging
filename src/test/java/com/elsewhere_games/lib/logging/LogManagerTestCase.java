83f5ed2ae281878bd1e9e8e4918538b34b357bdc
Hans Pragt <hpragt@gmail.com>
2013-10-10 22:06:12 -0700

    Adds a test to make sure multiple appenders are cached by the log mananger.

--- src/test/java/com/elsewhere_games/lib/logging/LogManagerTestCase.java
+++ src/test/java/com/elsewhere_games/lib/logging/LogManagerTestCase.java
@@ -1,8 +1,11 @@
 package com.elsewhere_games.lib.logging;
 
-import com.elsewhere_games.lib.logging.mock.MockAppender;
+// JUnit
 import org.junit.Assert;
 import org.junit.Test;
+
+// Logging Mocks
+import com.elsewhere_games.lib.logging.mock.MockAppender;
 
 /**
  * <p>A test case for the {@link LogManager} class.</p>
@@ -19,8 +22,8 @@ public class LogManagerTestCase {
 
 	@Test
 	public void appendersAreAppliedToExistingLogs() {
-		String logName = "new_log";
-		Log log = LogManager.getInstance().getLog(logName);
+		String name = "new_log";
+		Log log = LogManager.getInstance().getLog(name);
 		log.setLevel(Level.DEBUG);
 
 		MockAppender appender = new MockAppender();
@@ -46,8 +49,9 @@ public class LogManagerTestCase {
 		MockAppender appender = new MockAppender();
 		LogManager.getInstance().addAppender(appender);
 
-		String logName = "new_log";
-		Log log = LogManager.getInstance().getLog(logName);
+		String name = "new_log";
+		Log log = LogManager.getInstance().getLog(name);
+		log.setLevel(Level.DEBUG);
 
 		String message = "Test message";
 		log.debug(message);
@@ -55,6 +59,30 @@ public class LogManagerTestCase {
 		Assert.assertEquals(log, appender.getLastSource());
 		Assert.assertEquals(Level.DEBUG, appender.getLastLevel());
 		Assert.assertTrue(appender.getLastMessage().equals(message));
+	}
+
+	@Test
+	public void multipleAppendersAreAppliedToNewLogs() {
+		MockAppender appender1 = new MockAppender();
+		LogManager.getInstance().addAppender(appender1);
+
+		MockAppender appender2 = new MockAppender();
+		LogManager.getInstance().addAppender(appender2);
+
+		String name = "new_log";
+		Log log = LogManager.getInstance().getLog(name);
+		log.setLevel(Level.DEBUG);
+
+		String message = "Test message";
+		log.debug(message);
+
+		Assert.assertEquals(log, appender1.getLastSource());
+		Assert.assertEquals(Level.DEBUG, appender1.getLastLevel());
+		Assert.assertTrue(appender1.getLastMessage().equals(message));
+
+		Assert.assertEquals(log, appender2.getLastSource());
+		Assert.assertEquals(Level.DEBUG, appender2.getLastLevel());
+		Assert.assertTrue(appender2.getLastMessage().equals(message));
 	}
 
 	@Test
